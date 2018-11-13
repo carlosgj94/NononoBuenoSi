164321f94c57c307c1f9c8d0c0f872b0717c93de
carlosgj94 <carlosgj94@gmail.com>
2018-11-13 11:56:14 +0100

    Pushing the dist for github pages

---
+++ dist/js/about.8b907bfe.js
@@ -0,0 +1,6 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([['about'], {
+  f820(t, e, n) {
+    n.r(e); const a = function () { const t = this; const e = t.$createElement; t._self._c; return t._m(0); }; const s = [function () { const t = this; const e = t.$createElement; const n = t._self._c || e; return n('div', { staticClass: 'about' }, [n('h1', [t._v('This is an about page')])]); }]; const u = n('2877'); const i = {}; const o = Object(u.a)(i, a, s, !1, null, null, null); o.options.__file = 'About.vue'; e.default = o.exports;
+  },
+}]);
+// # sourceMappingURL=about.8b907bfe.js.map
